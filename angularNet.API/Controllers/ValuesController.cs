70233ce4ad09bef7ab63801ae538f69f4e8a451f
lgxkevin <lgx9587@gmail.com>
2019-02-23 16:40:01 +1300

    asyncAPI

--- angularNet.API/Controllers/ValuesController.cs
+++ angularNet.API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using angularNet.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace angularNet.API.Controllers
 {
@@ -17,17 +18,17 @@ namespace angularNet.API.Controllers
         }
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x=>x.Id==id);
+            var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
             return Ok(value);
         }
 
