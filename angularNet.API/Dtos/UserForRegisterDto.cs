efc6ee009baf621d1c98c76c71561491ac228471
lgxkevin <lgx9587@gmail.com>
2019-03-07 15:16:44 +1300

    validate username and password

--- angularNet.API/Dtos/UserForRegisterDto.cs
+++ angularNet.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace angularNet.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username{get;set;}
+        [Required]
+        [StringLength(8,MinimumLength = 4, ErrorMessage="Password must between 4 and 8 characters.")]
         public string Password{get;set;}
     }
 }
