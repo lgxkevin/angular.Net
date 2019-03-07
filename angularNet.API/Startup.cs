845537f31d778a5c01cbbc917719a84bca0e49bc
lgxkevin <lgx9587@gmail.com>
2019-03-07 13:42:53 +1300

    register service in startUp

--- angularNet.API/Startup.cs
+++ angularNet.API/Startup.cs
@@ -30,6 +30,8 @@ namespace angularNet.API
             services.AddDbContext<DataContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            //Services will be mmade once per request within the scope
+            services.AddScoped<IAuthRepository,AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
