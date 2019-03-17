8f971dc929f98c60d1c90bf44662d44c88adf26a
lgxkevin <lgx9587@gmail.com>
2019-03-17 18:34:23 +1300

    angular template forms

--- Angular-Front/src/app/app.module.ts
+++ Angular-Front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; 
+import {FormsModule} from '@angular/forms';
 
 
 
@@ -18,7 +19,8 @@ import { NavbarComponent } from './navbar/navbar.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
